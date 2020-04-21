910a035ec9371dc02274db713a89e5149e0c3e8f
Zaur Zulpikarov <Zulpikarov.Z@gmail.com>
2020-04-21 15:35:31 +0300

    react19 rework

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -3,7 +3,7 @@ import InputItem from '../InputItem/InputItem.js'
 import ItemList from '../ItemList/ItemList.js'
 import Footer from '../Footer/Footer.js'
 
-const todoItem = "Write new app";
+const todoItem = ["React", "Redux", "Immutable"];
 
 const App = () => (<div>
   <h1 style={{fontSize: 100, color: "gold"}}>todos</h1>
--- todo/src/components/ItemList/ItemList.js
+++ todo/src/components/ItemList/ItemList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Item from '../Item/Item.js';
 
 const ItemList = ({todoItem}) => (<ul style={{color: "olive"}}>
-  <li><Item todoItem={"React"} /></li>
-  <li><Item todoItem={"Redux"} /></li>
-  <li><Item todoItem={"Immutable"} /></li>
+  <li><Item todoItem={todoItem[0]} /></li>
+  <li><Item todoItem={todoItem[1]} /></li>
+  <li><Item todoItem={todoItem[2]} /></li>
 </ul>);
 
 export default ItemList;
