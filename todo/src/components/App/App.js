ba408dda16421cbe83932369512cedc916f6f95d
Zaur Zulpikarov <Zulpikarov.Z@gmail.com>
2020-04-27 22:04:19 +0300

    complete react22

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem.js'
 import ItemList from '../ItemList/ItemList.js'
 import Footer from '../Footer/Footer.js'
+import "./App.css"
+import "../ItemList/ItemList.css"
 
 // const todoItem = ["React", "Redux", "Immutable"];
 
@@ -13,8 +15,8 @@ const App = () => {
 	];
 
 	return (
-		<div>
-		  <h1 style={{fontSize: 100, color: "gold"}}>todos</h1>
+		<div className="wrap">
+		  <h1 className="wrap_title">todos</h1>
 		  <InputItem />
 		  <ItemList items={items} />
 		  <Footer count={3}/>
--- todo/src/components/ItemList/ItemList.js
+++ todo/src/components/ItemList/ItemList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Item from '../Item/Item.js';
 
-const ItemList = ({items}) => (<ul style={{color: "olive"}}>
+const ItemList = ({items}) => (<ul className="wrap_list">
 	{items.map(item => <li key={item.value}><Item value={item.value} /></li>)}
 </ul>);
 
