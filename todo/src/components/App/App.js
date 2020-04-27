a1378a1a1679fec03b44998dad808987c32668d6
Zaur Zulpikarov <Zulpikarov.Z@gmail.com>
2020-04-27 19:03:28 +0300

    complete react21

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -3,13 +3,21 @@ import InputItem from '../InputItem/InputItem.js'
 import ItemList from '../ItemList/ItemList.js'
 import Footer from '../Footer/Footer.js'
 
-const todoItem = ["React", "Redux", "Immutable"];
+// const todoItem = ["React", "Redux", "Immutable"];
 
-const App = () => (<div>
-  <h1 style={{fontSize: 100, color: "gold"}}>todos</h1>
-  <InputItem />
-  <ItemList todoItem={todoItem} />
-  <Footer count={3}/>
-</div>);
-      
+const App = () => {
+	const items = [
+		{value: "React"},
+		{value: "Redux"},
+		{value: "Immutable"}
+	];
+
+	return (
+		<div>
+		  <h1 style={{fontSize: 100, color: "gold"}}>todos</h1>
+		  <InputItem />
+		  <ItemList items={items} />
+		  <Footer count={3}/>
+		</div>);
+}
 export default App;
--- todo/src/components/ItemList/ItemList.js
+++ todo/src/components/ItemList/ItemList.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Item from '../Item/Item.js';
 
-const ItemList = ({todoItem}) => (<ul style={{color: "olive"}}>
-  <li><Item todoItem={todoItem[0]} /></li>
-  <li><Item todoItem={todoItem[1]} /></li>
-  <li><Item todoItem={todoItem[2]} /></li>
+const ItemList = ({items}) => (<ul style={{color: "olive"}}>
+	{items.map(item => <li key={item.value}><Item value={item.value} /></li>)}
 </ul>);
 
 export default ItemList;
