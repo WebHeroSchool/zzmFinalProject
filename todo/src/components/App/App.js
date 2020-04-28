898d507f1e34584d086717f5077087d0db06ded7
Zaur Zulpikarov <Zulpikarov.Z@gmail.com>
2020-04-28 20:25:47 +0300

    complete react23

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem.js'
 import ItemList from '../ItemList/ItemList.js'
 import Footer from '../Footer/Footer.js'
-import "./App.css"
-import "../ItemList/ItemList.css"
+import styles from './App.module.css'
 
-// const todoItem = ["React", "Redux", "Immutable"];
 
 const App = () => {
 	const items = [
@@ -15,8 +13,8 @@ const App = () => {
 	];
 
 	return (
-		<div className="wrap">
-		  <h1 className="wrap_title">todos</h1>
+		<div className={styles.wrap}>
+		  <h1 className={styles.title}>todos</h1>
 		  <InputItem />
 		  <ItemList items={items} />
 		  <Footer count={3}/>
--- todo/src/components/ItemList/ItemList.js
+++ todo/src/components/ItemList/ItemList.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Item from '../Item/Item.js';
+import styles from './ItemList.module.css'
 
-const ItemList = ({items}) => (<ul className="wrap_list">
+const ItemList = ({items}) => (<ul className={styles.list}>
 	{items.map(item => <li key={item.value}><Item value={item.value} /></li>)}
 </ul>);
 
