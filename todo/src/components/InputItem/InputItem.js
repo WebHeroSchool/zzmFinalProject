c30ce4dd005172d971873085359868f71e86f1e8
Zaur Zulpikarov <Zulpikarov.Z@gmail.com>
2020-04-30 11:04:27 +0300

    complete react25

--- todo/src/components/InputItem/InputItem.js
+++ todo/src/components/InputItem/InputItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<input placeholder={"What need to be done?"} />);
+const InputItem = () => (<div>
+	<TextField
+		id="outlined-basic"
+		label="What need to be done?"
+		variant="outlined"
+	/>
+</div>);
 
 export default InputItem;
