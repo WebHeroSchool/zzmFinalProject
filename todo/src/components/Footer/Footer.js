c3e8af6495c1683b8381f49c60b6096606d2f5e5
Zaur Zulpikarov <Zulpikarov.Z@gmail.com>
2020-05-05 14:05:34 +0300

    complete react26

--- todo/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = ({count}) => (<div>
-	<div>{count} items left</div>
-	<div></div>
-	<div>btn</div>
-</div>);
-
-export default Footer;
--- todo/src/components/ItemList/ItemList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Item from '../Item/Item.js';
-
-const ItemList = ({ items }) => (<ul>
-	{items.map(item => <li key={item.value}>
-		<Item value={item.value} isDone={item.isDone} /></li>)}
-</ul>);
-
-export default ItemList;
